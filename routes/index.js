15f47b4e9b33cf48e4b88a440039daa7fcbcdd3e
ANIL KUMAR <63443121+AndoAnil@users.noreply.github.com>
2021-06-24 21:43:35 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,40 @@
 
 //Twilo Message Code
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
 const port = 3000;
+
+const userDatabase = [];
+
+// Create user endpoint
+app.post('/users', (req, res) => {
+  const { email, password, phone } = req.body;
+  const user = {
+    email,
+    password,
+    phone
+  };
+
+  userDatabase.push(user);
+
+  res.status(201).send({
+    message: 'Account created successfully, kindly check your phone to activate your account!',
+    data: user
+  })
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-module.exports = app;
-//End Twilo Message Code
+module.exports = app;//End Twilo Message Code
 
 
 // Deps
