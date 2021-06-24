ef2ed82257173c50e0ecad4a99a7b01469c74816
ANIL KUMAR <63443121+AndoAnil@users.noreply.github.com>
2021-06-24 21:41:36 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,17 @@
 'use strict';
 
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+module.exports = app;
+
 // Deps
 var activity = require('./activity');
 
