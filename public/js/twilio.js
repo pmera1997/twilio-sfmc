46a029340a64f5867f81103ad15b2d16bd87e6e5
ANIL KUMAR <63443121+AndoAnil@users.noreply.github.com>
2021-06-24 21:50:11 +0530

    Update twilio.js

--- public/js/twilio.js
+++ public/js/twilio.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-
+const accountSid = 'ACefb6762e119ac9accff0d345636a6ee8';
+const authToken = '5db48de6e246012766ba0634ca4d22bd';
+const twilionumber='+16106098635';
 const sendSms = (phone, message) => {
   const client = require('twilio')(accountSid, authToken);
   client.messages
     .create({
        body: message,
-       from: process.env.TWILIO_PHONE_NUMBER,
+       from: twilionumber,
        to: phone
      })
     .then(message => console.log(message.sid));
