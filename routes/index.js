f435a99093ec885a09325a3f2526894c27477ad0
ANIL KUMAR <63443121+AndoAnil@users.noreply.github.com>
2021-06-25 10:32:07 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,46 +1,6 @@
 'use strict';
 
 //Twilo Message Code
-const express = require('express');
-const bodyParser = require('body-parser');
-const sendSms = require('./twilio');
-
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(bodyParser.json());
-
-const port = 3000;
-
-const userDatabase = [];
-
-// Create users endpoint
-app.post('/users', (req, res) => {
-  const { email, password, phone } = req.body;
-  const user = {
-    email,
-    password,
-    phone
-  };
-
-  userDatabase.push(user);
-
-  const welcomeMessage = 'Welcome to Chillz! Your verification code is 54875';
-
-  sendSms(user.phone, welcomeMessage);
-
-  res.status(201).send({
-    message: 'Ram Ram',
-    data: user
-  })
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-
-module.exports = app;
 
 //End Twilo Message Code
 
