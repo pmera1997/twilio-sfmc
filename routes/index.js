e25ad97e02c901569ba1b016c5e65b323bde7c48
ANIL KUMAR <63443121+AndoAnil@users.noreply.github.com>
2021-07-02 17:58:58 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,29 @@
-'use strict';
+//'use strict';
 
 //Twilo Message Code
+
+const path = require('path');
+const fs = require('fs');
+
+/**
+ * Render Config
+ * @param req
+ * @param res
+ */
+exports.config = (req, res) => {
+  const domain = req.headers.host || req.headers.origin;
+  const file = path.join(__dirname, '..', 'public', 'config-template.json');
+
+  const configTemplate = fs.readFileSync(file, 'utf-8');
+  const config = JSON.parse(configTemplate.replace(/\$DOMAIN/g, domain));
+  res.json(config);
+};
+
+/**
+ * Render UI
+ * @param req
+ * @param res
+ */
 
 //End Twilo Message Code
 
