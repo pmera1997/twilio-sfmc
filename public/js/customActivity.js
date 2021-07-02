bcbab0473fb1d144b378c8be870487795a0f5898
ANIL KUMAR <63443121+AndoAnil@users.noreply.github.com>
2021-07-02 21:35:26 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -8,11 +8,19 @@ define([
     var connection = new Postmonger.Session();
     var payload = {};
     var lastStepEnabled = false;
-    var steps = [ // initialize to the same value as what's set in config.json for consistency
-        { "label": "Create SMS Message", "key": "step1" }
+    
+   var steps = [ // initialize to the same value as what's set in config.json for consistency
+        { "label": "First Step", "key": "step1" },
+        { "label": "Second Step", "key": "step2" },
+        { "label": "Third Step", "key": "step3" },
+        { "label": "Final Step", "key": "step4" }
     ];
+    
     var currentStep = steps[0].key;
-
+    connection.on('clickedNext', onClickedNext);
+    connection.on('clickedBack', onClickedBack);
+    connection.on('gotoStep', onGotoStep);
+    
     $(window).ready(onRender);
 
     connection.on('initActivity', initialize);
@@ -30,6 +38,7 @@ define([
         connection.trigger('requestEndpoints');
     }
 
+    
   function initialize(data) {
         console.log("Initializing data data: "+ JSON.stringify(data));
         if (data) {
@@ -77,6 +86,80 @@ define([
 
     }
 
+    //multiwizard setup
+    function onClickedNext () {
+        if (currentStep.key === 'step4') 
+        {
+            save();
+        }
+        else
+        {
+            connection.trigger('nextStep');
+        }
+    }
+
+    function onClickedBack () 
+    {
+        connection.trigger('prevStep');
+    }
+    function onGotoStep (step) {
+        showStep(step);
+        connection.trigger('ready');
+    }
+
+    function showStep(step, stepIndex) {
+        if (stepIndex && !step) {
+            step = steps[stepIndex-1];
+        }
+
+        currentStep = step;
+
+        $('.step').hide();
+
+        switch(currentStep.key) {
+            case 'step1':
+                $('#step1').show();
+                connection.trigger('updateButton', {
+                    button: 'next',
+                    visible: false
+                });
+                connection.trigger('updateButton', {
+                    button: 'back',
+                    visible: false
+                });
+                break;
+            case 'step2':
+                $('#step2').show();
+                connection.trigger('updateButton', {
+                    button: 'back',
+                    visible: true
+                });
+                connection.trigger('updateButton', {
+                    button: 'next',
+                    text: 'next',
+                    visible: true
+                });
+                break;
+            case 'step3':
+                $('#step3').show();
+                connection.trigger('updateButton', {
+                    button: 'back',
+                    visible: true
+                });
+                connection.trigger('updateButton', {
+                    button: 'next',
+                    text: 'next',
+                    visible: true
+                });
+                break;
+            case 'step4':
+                $('#step4').show();
+                break;
+        }
+    }
+
+    
+    //multiwizard End
     function onGetTokens (tokens) {
         // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
         console.log("Tokens function: "+JSON.stringify(tokens));
