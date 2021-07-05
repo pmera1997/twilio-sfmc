3c845fdea4ceddf489e5db510b27a749c293f0ce
ANIL KUMAR <63443121+AndoAnil@users.noreply.github.com>
2021-07-05 18:44:47 +0530

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -17,10 +17,11 @@ const alignColorsAndTime = winston.format.combine(
   }),
   winston.format.timestamp({
     format: 'YY-MM-DD HH:MM:SS',
-  }),
-  winston.format.printf(
-    (info) => ` ${info.label}  ${info.timestamp}  ${info.level} : ${info.message}`,
-  ),
+  })
+  //,
+  //winston.format.printf(
+   // (info) => ` ${info.label}  ${info.timestamp}  ${info.level} : ${info.message}`,
+  //),
 );
 
 logger.add(new (winston.transports.Console)({
