3709211a3498f692eb6210349fa7b7f4da6cbfae
ANIL KUMAR <63443121+AndoAnil@users.noreply.github.com>
2021-07-05 19:54:44 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -132,19 +132,25 @@ exports.execute = function (req, res){
                console.log(JSON.stringify(message));
                console.log("Account SID:"+message.accountSid);
                console.log("apiVersion:"+message.apiVersion);
-        
-                SFClient.saveData(process.env.DATA_EXTENSION_EXTERNAL_KEY, [
-                  {
-                    keys: {
-                      uri: message.uri,
-                    },
-                    values: {
-                      uri: message.uri,  
-                      account_sid: message.account_sid,
-                      api_version: message.api_version,
-                    },
-                  },
-                ]);
+               try
+               {
+                    SFClient.saveData(process.env.DATA_EXTENSION_EXTERNAL_KEY, [
+                      {
+                        keys: {
+                          uri: message.uri,
+                        },
+                        values: {
+                          uri: message.uri,  
+                          account_sid: message.account_sid,
+                          api_version: message.api_version,
+                        },
+                      },
+                    ]);
+               }
+                catch(err)   
+               {
+                   console.log(err);
+               }
         
             }) 
           .done();
