b1b8d6601eb9b857b98f3efaf1737528f5d60d61
ANIL KUMAR <63443121+AndoAnil@users.noreply.github.com>
2021-07-06 17:58:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.post('/journeybuilder/save/', activity.save );
 app.post('/journeybuilder/validate/', activity.validate );
 app.post('/journeybuilder/publish/', activity.publish );
 app.post('/journeybuilder/execute/', activity.execute );
-app.post('/journeybuilder/message/', activity.message );
+//app.post('/journeybuilder/message/', activity.message );
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
