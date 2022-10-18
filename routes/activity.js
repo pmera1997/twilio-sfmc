5af5c1c4bfd4af70dac04d51d88123c630ae546f
pmera1997 <114147426+pmera1997@users.noreply.github.com>
2022-10-18 17:50:58 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -125,21 +125,8 @@ exports.execute = function (req, res) {
 
     //----Sent Request
 
-
-
-
-//---------------
-
-   // Download the helper library from https://www.twilio.com/docs/node/install
-// Find your Account SID and Auth Token at twilio.com/console
-// and set the environment variables. See http://twil.io/secure
-  const accountSid = requestBody.accountSid;
-  const authToken = requestBody.authToken;
   const client = require('twilio')(accountSid, authToken);
 
-client.messages
-      .create({body: body , from: from, to: to })
-      .then(message => console.log(message.sid));
 
 
 
